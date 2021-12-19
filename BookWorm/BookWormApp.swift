1b64cfe4d6388a25f8e2013c2312bc479f924074
VIctor <colen.victor@icloud.com>
2021-12-19 07:01:35 -0300

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Victor Colen on 19/12/21.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
